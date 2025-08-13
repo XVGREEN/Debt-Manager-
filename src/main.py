53ff9570be9266121058fe63f2ab645c848eb205
XVGREEN <cebolazacarias18@gmail.com>
2025-08-13 09:14:40 +0200

    code cleanup

--- src/main.py
+++ src/main.py
@@ -7,45 +7,56 @@ emanuel=Sujeito("87255223",83000000)
 dividas = [Divida(cebola,"20/08/2025",15),  
           Divida(agostinho,"20/08/2025",20000),
           Divida(emanuel,"20/08/2025",0.01)]
-          
+
+
+def listar():
+    print("Listando todas dividas:")
+    for i in dividas:
+        print(i)
+    input("pressione qualquer tecla pra prosseguir...")
+
+
+def pesquisar():
+    nome = input("Escreva o nome da pessoa que deve: ")        
+    encontrou = False
+    for divida in dividas:
+        if divida.sujeito.nome == nome:
+            print(divida)
+            encontrou = True
+        if not encontrou:
+           print("Nenhuma dívida encontrada para esse nome.")
+    
+def cadastrar():
+    nome = input("Escreva o nome da pessoa:")
+    numero = input("Escreva o numero:")
+    prazo = input("Escreva o prazo:")
+    valor = int(input("Escreva o valor:"))
+    s=Sujeito(nome,numero)
+    d =Divida(s,prazo,valor)
+    dividas.append(d)
+
+      
 def show_menu():
     print (".............MeuTaku..........\n")
-    print("1.Ver dividas\n")
+    print("1.Listar dividas\n")
     print("2.Pesquisar \n")
-    print("3.Criar nova divida\n")
-    print("4.Eliminar divida\n")
+    print("3.Cadastrar  Divida\n")
+    print("4.Eliminar Divida\n")
     print("5.Sair\n")
     resposta = int(input("Escolha uma opcao:"))
     print("\033c", end="")
-
     if resposta==1:
-        print("Listando todas dividas:")
-        for i in dividas:
-            print(i)
-        input("pressione qualquer tecla pra prosseguir...")
+        listar()
         print("\033c", end="")
         show_menu()           
     if resposta==2:
-        nome = input("Escreva o nome da pessoa que deve: ")
-        encontrou = False
-        for divida in dividas:
-            if divida.sujeito.nome == nome:
-                print(divida)
-                encontrou = True
-        if not encontrou:
-            print("Nenhuma dívida encontrada para esse nome.")
+        pesquisas()
         input("Pressione qualquer tecla para prosseguir...")
         print("\033c", end="")
         show_menu()
 
     if resposta == 3:
-        nome = input("Escreva o nome da pessoa:")
-        numero = input("Escreva o numero:")
-        prazo = input("Escreva o prazo:")
-        valor = int(input("Escreva o valor:"))
-        s=Sujeito(nome,numero)
-        d =Divida(s,prazo,valor)
-        dividas.append(d)
+        cadastrar()
         print("\033c", end="")
         show_menu()
     if resposta == 4:
